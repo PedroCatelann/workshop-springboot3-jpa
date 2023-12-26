409e6f4e3701e25ed737ac0276fd5402d3c70db3
PedroCatelann <pedrohfktlan@gmail.com>
2023-12-26 20:55:23 -0300

    order parte 1

--- src/main/java/com/pedrocatelan/course/entities/User.java
+++ src/main/java/com/pedrocatelan/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.pedrocatelan.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 		
 	}
@@ -34,6 +40,14 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
 	public Long getId() {
