772daededa22732870264c2327dd05764eaa5dec
PedroCatelann <pedrohfktlan@gmail.com>
2023-12-26 21:29:52 -0300

    order parte 2

--- src/main/java/com/pedrocatelan/course/entities/Order.java
+++ src/main/java/com/pedrocatelan/course/entities/Order.java
@@ -21,6 +21,7 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private Instant moment;
 	
 	@ManyToOne
--- src/main/java/com/pedrocatelan/course/entities/User.java
+++ src/main/java/com/pedrocatelan/course/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +19,7 @@ import jakarta.persistence.Table;
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+	 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,6 +28,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
