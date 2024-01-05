080ec8c0d2fcaff7d415f58d98c44b313c9ea1f9
PedroCatelann <pedrohfktlan@gmail.com>
2024-01-05 11:41:27 -0300

    OrderItem, many-yo-many association with extra attributes

--- src/main/java/com/pedrocatelan/course/entities/Category.java
+++ src/main/java/com/pedrocatelan/course/entities/Category.java
@@ -9,8 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -24,7 +24,8 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@ManyToMany(mappedBy = "categories")
+	
 	private Set<Product> product = new HashSet<>();
 	
 	public Category() {
