7fa1a17ae7525f7b6dddb6c53dee3330940223b7
PedroCatelann <pedrohfktlan@gmail.com>
2024-01-10 16:18:37 -0300

    Tratamento de exceção - findById

--- src/main/java/com/pedrocatelan/course/services/UserService.java
+++ src/main/java/com/pedrocatelan/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.pedrocatelan.course.entities.User;
 import com.pedrocatelan.course.repositories.UserRepository;
+import com.pedrocatelan.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
