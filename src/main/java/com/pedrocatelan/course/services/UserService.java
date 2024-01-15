ab53d1a37669de88b5ebd0ee65e60f544a9afbb5
PedroCatelann <pedrohfktlan@gmail.com>
2024-01-15 09:12:54 -0300

    Update Exception

--- src/main/java/com/pedrocatelan/course/services/UserService.java
+++ src/main/java/com/pedrocatelan/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.pedrocatelan.course.repositories.UserRepository;
 import com.pedrocatelan.course.services.exceptions.DatabaseException;
 import com.pedrocatelan.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -48,10 +50,13 @@ public class UserService {
 
 	public User update(Long id, User obj) {
 
-		User entity = repository.getReferenceById(id);
-
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 
 	}
 
