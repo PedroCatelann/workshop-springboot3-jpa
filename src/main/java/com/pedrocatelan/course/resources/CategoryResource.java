17807575f7ddd8206d21d074804bdf9443fdef25
PedroCatelann <pedrohfktlan@gmail.com>
2024-02-22 17:41:42 -0300

    adding endpoints post, put, delete for categories

--- src/main/java/com/pedrocatelan/course/resources/CategoryResource.java
+++ src/main/java/com/pedrocatelan/course/resources/CategoryResource.java
@@ -1,16 +1,15 @@
 package com.pedrocatelan.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.pedrocatelan.course.entities.Category;
 import com.pedrocatelan.course.services.CategoryService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -31,6 +30,25 @@ public class CategoryResource {
 		Category obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 } 
--- src/main/java/com/pedrocatelan/course/services/CategoryService.java
+++ src/main/java/com/pedrocatelan/course/services/CategoryService.java
@@ -3,7 +3,11 @@ package com.pedrocatelan.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.pedrocatelan.course.services.exceptions.DatabaseException;
+import com.pedrocatelan.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pedrocatelan.course.entities.Category;
@@ -24,5 +28,32 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		
 		return obj.get();
+	}
+
+	public Category insert(Category category) {
+		return repository.save(category);
+	}
+
+	public Category update(Long id, Category category) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateData(entity, category);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(e);
+		}
+	}
+
+	private void updateData(Category entity, Category category) {
+		entity.setName(category.getName());
+	}
+
+	public void delete(Long id) {
+		try {
+			if(repository.existsById(id)) repository.deleteById(id);
+			else throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
